800c576cc6f451ee9abc3ed9ed71df23854e030f
Anna-Sosiura <132984503+Anna-Sosiura@users.noreply.github.com>
2023-10-13 02:06:54 +0300

    goit-js-hw-10(2)

--- src/sass/js/cat-api.js
+++ src/sass/js/cat-api.js
@@ -4,23 +4,14 @@ axios.defaults.headers.common["x-api-key"] = "live_LGPzLNmcfsIn0lJSb4741HxK6tV5z
 axios.defaults.baseURL = 'https://api.thecatapi.com/';
 
 export async function fetchBreeds() {
-    try {
       const { data } = await axios('v1/breeds');
       return data;
-    } catch (error) {
-      refs.error.classList.remove('is-hidden')
-      Notify.failure(error)
-    }
   };
 
   export async function fetchCatByBreed(breedId) {
-    try {
+    
       const { data } = await axios(`v1/images/search?breed_ids=${breedId}`);
       return data;
-    } catch (error) {
-      refs.error.classList.remove('is-hidden')
-      Notify.failure(error);
-    }
   };
 
 
--- src/sass/js/index.js
+++ src/sass/js/index.js
@@ -3,8 +3,6 @@ import SlimSelect from 'slim-select';
 import "slim-select/dist/slimselect.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
-
 axios.defaults.headers.common["x-api-key"] = "live_LGPzLNmcfsIn0lJSb4741HxK6tV5zDOjt6YiVMAivR46ccn3ITJooEWMlPGqPozE";
 import {
   fetchBreeds,
@@ -17,42 +15,49 @@ import {
     error:document.querySelector('.error'),
     catInfo:document.querySelector('.cat-info'),
   }
-  
   refs.loader.classList.add('is-hidden');
   refs.error.classList.add('is-hidden');
   refs.catInfo.classList.add('is-hidden');
+  
 
   const breedsArray=[];
+
   fetchBreeds().then(data=>{data.forEach(element => {breedsArray.push({
     text:element.name, value:element.id})});
     new SlimSelect({
       select: refs.breedSelect,
       data: breedsArray,
-    });})
-
+    });
+  });
+  
     refs.breedSelect.addEventListener('change',onSelect);
-    
+   
     function onSelect(event){
-      
-      refs.loader.classList.replace('is-hidden','loading');
+      refs.loader.classList.replace('is-hidden', 'loading');
       
       const breedId=event.currentTarget.value;
-      try {
         fetchCatByBreed(breedId)
+        
       .then(data=>{
+
+
         refs.loader.classList.replace('loading','is-hidden');
-      refs.breedSelect.classList.remove('is-hidden');
-      refs.catInfo.classList.remove('is-hidden');
       refs.catInfo.innerHTML=`<div><h2>${data[0].breeds[0].name}</h2><img src="${data[0].url}" alt="${data[0].breeds[0].name}" width="700"/>
       <p>${data[0].breeds[0].temperament}</p>
       <p class="description">${data[0].breeds[0].description}</p>
-      </div>`
+      </div>`;
+      refs.catInfo.classList.remove('is-hidden');
     })
-      } catch (error) {
-        refs.error.classList.remove('is-hidden')
-        Notify.failure(error);
+      .catch (onError) 
       }
-    };
+      function onError(){
+        refs.loader.classList.replace('loading','is-hidden');
+        refs.breedSelect.classList.remove('is-hidden');
+        refs.catInfo.classList.add('is-hidden');
+        Notify.failure(`Oops! Something went wrong! Try reloading the page!`);
+      }
+    
+    
 
 
 
